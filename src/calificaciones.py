13e53400406051a0c13db2472b0c2a3d36750bd8
Julian Martinez <debian@debian.debian>
2026-08-27 18:14:24 -0400

    fix: valida el rango de las calificaciones

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,4 +1,9 @@
 def calcular_promedio(calificaciones):
     if len(calificaciones) == 0:
-        raise ValueError("La lista no puede estar vacia")
-    return sum(calificaciones ) / len(calificaciones)
+        raise ValueError("La lista no puede estar vacía")
+
+    for nota in calificaciones:
+        if nota < 0.0 or nota > 5.0:
+            raise ValueError("Calificación fuera de rango")
+
+    return sum(calificaciones) / len(calificaciones)
