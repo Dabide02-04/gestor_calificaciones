613dca7f4677e7bbf6a8f35e57c5b11d3c4c4de9
Julian Martinez <debian@debian.debian>
2026-08-27 18:28:26 -0400

    test: agrega pruebas para las notas extremas

--- tests/test_calificaciones.py
+++ tests/test_calificaciones.py
@@ -1,4 +1,4 @@
-from src.calificaciones import calcular_promedio, determinar_estado
+from src.calificaciones import calcular_promedio, determinar_estado, obtener_nota_mayor, obtener_nota_menor
 import pytest
 def test_calcular_promedio_de_tres_notas():
     resultado = calcular_promedio([4.0,3.0, 5.0])
@@ -25,3 +25,21 @@ def test_determinar_estado_aprobado():
 def test_determinar_estado_reprobado():
     resultado = determinar_estado([2.0, 3.0, 2.0])
     assert resultado == "Reprobado"
+    
+def test_obtener_nota_mayor():
+    resultado = obtener_nota_mayor([4.0, 3.0, 5.0, 3.5])
+    assert resultado == 5.0
+
+
+def test_obtener_nota_menor():
+    resultado = obtener_nota_menor([4.0, 3.0, 5.0, 3.5])
+    assert resultado == 3.0
+
+def test_obtener_nota_mayor_lista_vacia():
+    with pytest.raises(ValueError):
+        obtener_nota_mayor([])
+
+
+def test_obtener_nota_menor_lista_vacia():
+    with pytest.raises(ValueError):
+        obtener_nota_menor([])
