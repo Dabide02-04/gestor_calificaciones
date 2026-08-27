92371447bda27747a86dc396f3b6d3ae74eda581
Julian Martinez <debian@debian.debian>
2026-08-27 18:00:05 -0400

    fix: controla el calculo con lista vacia

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,2 +1,4 @@
 def calcular_promedio(calificaciones):
+    if len(calificaciones) == 0:
+        raise ValueError("La lista no puede estar vacia")
     return sum(calificaciones ) / len(calificaciones)
